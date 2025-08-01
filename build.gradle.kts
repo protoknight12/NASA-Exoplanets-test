d43e8a4839485faf9a150012c7dfeab299be4753
damqn4etoBG <79470838+damqn4etoBG@users.noreply.github.com>
2025-08-01 21:02:18 +0300

    Added libraries + refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,33 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "net.officer"
+version = "1.0"
+
+val lwjglVersion = "3.3.6"
+val lwjglNatives = "natives-windows"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // Core LWJGL modules
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-stb:$lwjglVersion")
+
+    // Native bindings (important!)
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-stb:$lwjglVersion:$lwjglNatives")
+
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("io.github.spair:imgui-java-app:1.89.0")
+    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("io.github.cdimascio:dotenv-java:3.2.0")
 }
 
 tasks.test {
