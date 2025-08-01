d850324ef660add4da75724c8115d70979658eff
Officer <84569377+protoknight12@users.noreply.github.com>
2025-08-02 00:58:15 +0300

    statistics averaging

--- src/main/java/net/officer/exoplanets/RequestGetter.java
+++ src/main/java/net/officer/exoplanets/RequestGetter.java
@@ -1,10 +1,14 @@
 package net.officer.exoplanets;
 
+import com.google.gson.*;
+
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RequestGetter {
@@ -30,5 +34,61 @@ public class RequestGetter {
             }
         }
         in.close();
+        averagePlanetInfo(planetName);
+    }
+
+    private static void averagePlanetInfo(String planetName) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        File file = new File("src/main/resources/data/" + planetName + ".json");
+        try (Reader reader = new FileReader(file)) {
+            JsonArray array = JsonParser.parseReader(reader).getAsJsonArray();
+            if (array != null) {
+                String hostName = array.asList().getFirst().getAsJsonObject().get("hostname").getAsString();
+                List<Double> orbsMax = new ArrayList<>();
+                List<Double> orbIncl = new ArrayList<>();
+                List<Double> mass = new ArrayList<>();
+                List<Double> radius = new ArrayList<>();
+                List<Double> rightAsc = new ArrayList<>();
+                List<Double> declination = new ArrayList<>();
+
+                for (JsonElement elem : array) {
+                    JsonObject jsonObj = elem.getAsJsonObject();
+                    double orbsMax1 = getDoubleOrDefault(jsonObj,"pl_orbsmax");
+                    double orbIncl1 = getDoubleOrDefault(jsonObj,"pl_orbincl");
+                    double mass1 = getDoubleOrDefault(jsonObj,"st_mass");
+                    double radius1 = getDoubleOrDefault(jsonObj,"st_rad");
+                    double rightAsc1 = getDoubleOrDefault(jsonObj,"ra");
+                    double declination1 = getDoubleOrDefault(jsonObj,"dec");
+
+                    orbsMax.add(orbsMax1);
+                    orbIncl.add(orbIncl1);
+                    mass.add(mass1);
+                    radius.add(radius1);
+                    rightAsc.add(rightAsc1);
+                    declination.add(declination1);
+                }
+                JsonArray avrgStats = new JsonArray();
+                JsonObject planet = new JsonObject();
+                planet.addProperty("pl_name", planetName);
+                planet.addProperty("hostname", hostName);
+                planet.addProperty("pl_orbsmax", orbsMax.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+                planet.addProperty("pl_orbincl", orbIncl.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+                planet.addProperty("st_mass", mass.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+                planet.addProperty("st_rad", radius.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+                planet.addProperty("ra", rightAsc.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+                planet.addProperty("dec", declination.stream().mapToDouble(Double::doubleValue).average().orElse(Double.NaN));
+
+                avrgStats.add(planet);
+                try (Writer writer = new FileWriter("src/main/resources/data/" + planetName + "(avrg)" + ".json")) {
+                    gson.toJson(avrgStats, writer);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static double getDoubleOrDefault(JsonObject object, String key) {
+        return object.has(key) && !object.get(key).isJsonNull() ? object.get(key).getAsDouble() : 1d;
     }
 }
